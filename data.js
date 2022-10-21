5ad0add62b7a07ac32629cf31a3fd4d173348070
sangyupkim <116325918+sangyupkim@users.noreply.github.com>
2022-10-21 13:01:16 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,175 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-3_1---",
+      "name": "3_1 - 파노라마",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-3_2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-3_2---",
+      "name": "3_2 - 파노라마",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3876978640782784,
+          "pitch": -0.2425168065389105,
+          "rotation": 0,
+          "target": "2-3_3---"
+        },
+        {
+          "yaw": 3.1035168460700824,
+          "pitch": 0.039081563425746424,
+          "rotation": 0,
+          "target": "0-3_1---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3_3---",
+      "name": "3_3 - 파노라마",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7830292125508187,
+          "pitch": -1.3505737723259532,
+          "rotation": 0,
+          "target": "3-3_4---"
+        },
+        {
+          "yaw": -0.021112087301043303,
+          "pitch": 0.5603339756117869,
+          "rotation": 0,
+          "target": "1-3_2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-3_4---",
+      "name": "3_4 - 파노라마",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0840488463847056,
+          "pitch": 1.4949098798580351,
+          "rotation": 0,
+          "target": "2-3_3---"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
